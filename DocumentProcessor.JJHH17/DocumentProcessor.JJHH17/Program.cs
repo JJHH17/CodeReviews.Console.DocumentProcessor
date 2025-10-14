ff1942b182ba2a9e7a1c0ca920ee2aa84be5e09a
James Hatfield <james.hatfield17@icloud.com>
2025-10-14 13:17:32 +0100

    Added initial program.cs class, includes spectre menu

---
+++ DocumentProcessor.JJHH17/DocumentProcessor.JJHH17/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Spectre.Console;
+
+namespace DocumentProcessor.JJHH17;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Menu();
+    }
+
+    enum MenuOptions
+    {
+        AddEntry,
+        Read,
+        Delete,
+        Update,
+        Exit
+    }
+
+    public static void Menu()
+    {
+        bool running = true;
+        while (running)
+        {
+            AnsiConsole.MarkupLine("[bold yellow]Document Processor Menu[/]");
+            Console.Clear();
+
+            var choice = AnsiConsole.Prompt(
+                new SelectionPrompt<MenuOptions>()
+                    .Title("Select an option:")
+                    .AddChoices(Enum.GetValues<MenuOptions>()));
+
+            switch (choice)
+            {
+                case MenuOptions.AddEntry:
+                    Console.WriteLine("Add Entry selected.");
+                    Console.ReadKey();
+                    break;
+
+                case MenuOptions.Read:
+                    Console.WriteLine("Read selected.");
+                    Console.ReadKey();
+                    break;
+
+                case MenuOptions.Delete:
+                    Console.WriteLine("Delete selected.");
+                    Console.ReadKey();
+                    break;
+
+                case MenuOptions.Update:
+                    Console.WriteLine("Update selected.");
+                    Console.ReadKey();
+                    break;
+
+                case MenuOptions.Exit:
+                    Console.WriteLine("Press any key to exit...");
+                    Console.ReadKey();
+                    running = false;
+                    break;
+            }
+        }
+    }
+}
