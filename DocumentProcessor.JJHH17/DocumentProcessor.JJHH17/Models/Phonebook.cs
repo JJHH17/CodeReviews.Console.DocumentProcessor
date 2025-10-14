67ed932a1a9cce46f42e512b4e054681826e4e73
James Hatfield <james.hatfield17@icloud.com>
2025-10-14 13:34:42 +0100

    Created initial program model and DB Context

---
+++ DocumentProcessor.JJHH17/DocumentProcessor.JJHH17/Models/Phonebook.cs
@@ -0,0 +1,26 @@
+﻿using System.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace DocumentProcessor.JJHH17.Models;
+
+public class Phonebook
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+}
+
+public class PhoneBookContext : DbContext
+{
+    private static readonly string server = ConfigurationManager.AppSettings["Server"];
+    private static readonly string databaseInstance = ConfigurationManager.AppSettings["DatabaseInstance"];
+    public static string connectionString = $@"Server=({server})\{databaseInstance};Integrated Security=true;";
+
+    public DbSet<Phonebook> Phonebooks { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(connectionString);
+    }
+}
